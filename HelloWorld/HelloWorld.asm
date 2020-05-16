e8fd19ebda6d69266bfdb3ef602c0dd5e2000179
3lif1 <alharrsiali@gmail.com>
2020-05-15 22:04:29 -0700

    v1.0

--- HelloWorld/HelloWorld.asm
+++ HelloWorld/HelloWorld.asm
@@ -2,11 +2,11 @@
 ;Os: Ubuntu
 ;*************************************************
 ; nasm -f elf32 -o HelloWorld.o HelloWorld.asm
-; ld -o HelloWorld HelloWorlld
+; ld -o HelloWorld HelloWorld.o
 ;*************************************************
 global _start
 
-section: .text
+section .text
 _start:
     mov eax, 0x4 ; sys call write
     mov ebx, 0x1 ; first arg: std_out => 1
@@ -21,6 +21,6 @@ _start:
     int 0x80
 
 
-section: .data
+section .data
     message: db "Hello world!" ; db: define bytes to store the string
     mlen:    equ $-message ; equ: away that nasm understand to store message length
